cdee9a84a6dee43d7b4b8de3f52e56cb59c412a4
Rahel Lüthy <rahel.luethy@gmail.com>
2014-07-03 16:49:08 +0200

    Keeping it simple

--- nemo/fish_prompt.fish
+++ nemo/fish_prompt.fish
@@ -1,10 +1,8 @@
 # Based on 'Informative Git Prompt' by Mariusz Smykula <mariuszs at gmail.com>
 
 set -g __fish_git_prompt_show_informative_status 'yes'
-set -g __fish_git_prompt_hide_untrackedfiles 1
 
 set -g __fish_git_prompt_color_branch magenta --bold
-set -g __fish_git_prompt_showupstream none # already using informative_status
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
 set -g __fish_git_prompt_char_upstream_prefix ""
@@ -21,27 +19,12 @@ set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 set -g __fish_git_prompt_color_cleanstate green --bold
 
-
-function fish_prompt --description 'Write out the prompt'
-
-  set -l last_status $status
-
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-  end
-
-  # PWD
-  set_color $fish_color_cwd
-  echo -n (pwd)
-  set_color normal
-
-  printf '%s ' (__fish_git_prompt)
-
-  if not test $last_status -eq 0
-  set_color $fish_color_error
-  end
-
-  echo -n '$ '
-
-  set_color normal
+function fish_prompt
+ set last_status $status
+ set_color $fish_color_cwd
+ printf '%s' (pwd)
+ set_color normal
+ printf '%s ' (__fish_git_prompt)
+ set_color normal
+ echo -n '$ '
 end
