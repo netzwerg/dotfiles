d74eee7ec3460c09c35f365fb29a9e1575f2a261
Rahel Lüthy <rahel.luethy@gmail.com>
2014-07-03 15:32:53 +0200

    Further fish tweaking

--- config.fish
+++ config.fish
@@ -1,6 +1,19 @@
+# Configure the fish
 set fish_path $HOME/.oh-my-fish
 set fish_theme nemo
 set fish_plugins git
+
+# Update the PATH to ensure Homebrew packages are used.
+set homebrew  "/usr/local/bin"
+set netzwerg  "$HOME/bin"
+set -gx PATH $netzwerg $homebrew $PATH
+
+# Set default text editor to Sublime.
+set -gx EDITOR 'subl -w'
+set -gx VISUAL 'subl -w'
+
+# Handy aliases
+alias e=$EDITOR
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
