f1f27e545466daddd62aa000ac6f36dd9159dff3
Rahel Lüthy <rahel.luethy@gmail.com>
2014-12-08 14:14:55 +0100

    Periodic backup

--- config.fish
+++ config.fish
@@ -7,10 +7,17 @@ set fish_plugins git
 set homebrew  "/usr/local/bin"
 set netzwerg  "$HOME/bin"
 set dev "$HOME/Dev"
-set -gx SCALA_HOME "$dev/scala-2.11.1"
-set -gx PATH $netzwerg $homebrew $SCALA_HOME/bin $PATH
 
-set --export EDITOR "vim"
+set -gx EDITOR "vim"
+set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home"
+set -gx SCALA_HOME "$dev/scala-2.11.1"
+set -gx XNAT_HOME "$HOME/Projects/xnat"
+
+set -gx DOCKER_HOST "tcp://192.168.59.103:2376"
+set -gx DOCKER_CERT_PATH "/Users/netzwerg/.boot2docker/certs/boot2docker-vm"
+set -gx DOCKER_TLS_VERIFY "1"
+
+set -gx PATH $netzwerg $homebrew $SCALA_HOME/bin $XNAT_HOME/bin $PATH
 
 # Handy aliases
 alias e=mvim
