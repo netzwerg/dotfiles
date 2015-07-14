0e139754a6c0413e8a7a2f2f86229e6a78f82623
Rahel Lüthy <rahel.luethy@gmail.com>
2015-07-14 06:53:32 +0200

    Update global env vars

--- config.fish
+++ config.fish
@@ -9,9 +9,8 @@ set netzwerg  "$HOME/bin"
 set dev "$HOME/Dev"
 
 set -gx EDITOR "vim"
-set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home"
+set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home"
 set -gx SCALA_HOME "$dev/scala-2.11.1"
-set -gx XNAT_HOME "$HOME/Projects/xnat"
 
 set -gx DOCKER_HOST "tcp://192.168.59.103:2376"
 set -gx DOCKER_CERT_PATH "/Users/netzwerg/.boot2docker/certs/boot2docker-vm"
