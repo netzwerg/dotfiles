f215862d1920018a23986c8daacbb6d8cff9e8ea
Rahel Lüthy <rahel.luethy@gmail.com>
2015-11-14 07:35:15 +0100

    Upgrade to latest oh-my-fish/oh-my-fish

--- config.fish
+++ config.fish
@@ -1,7 +1,8 @@
-# Configure the fish
-set fish_path $HOME/.oh-my-fish
-set fish_theme nemo
-set fish_plugins git
+# Path to Oh My Fish install.
+set -gx OMF_PATH /Users/netzwerg/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG /Users/netzwerg/dotfiles/omf-config
 
 # Update the PATH to ensure Homebrew packages are used.
 set homebrew  "/usr/local/bin"
@@ -9,12 +10,14 @@ set netzwerg  "$HOME/bin"
 set dev "$HOME/Dev"
 
 set -gx EDITOR "vim"
-set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home"
+set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home"
 set -gx SCALA_HOME "$dev/scala-2.11.1"
 
 set -gx DOCKER_HOST "tcp://192.168.59.103:2376"
 set -gx DOCKER_CERT_PATH "/Users/netzwerg/.boot2docker/certs/boot2docker-vm"
 set -gx DOCKER_TLS_VERIFY "1"
+
+set -gx GRADLE_OPTS "-Dfile.encoding=UTF-8"
 
 set -gx PATH $netzwerg $homebrew $SCALA_HOME/bin $XNAT_HOME/bin $PATH
 
@@ -26,4 +29,4 @@ alias st=stree
 alias find-note='mdfind -name notes.txt -onlyin ~ -0 | xargs -0 grep -i'
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+source $OMF_PATH/init.fish
