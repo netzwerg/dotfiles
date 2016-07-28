b6aaea80e1a30dc635212027e2e86d4e61b49c41
Rahel Lüthy <rahel.luethy@gmail.com>
2016-07-28 07:35:24 +0200

    Switch boot2docker to docker-machine

--- config.fish
+++ config.fish
@@ -10,12 +10,8 @@ set netzwerg  "$HOME/bin"
 set dev "$HOME/Dev"
 
 set -gx EDITOR "vim"
-set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_74.jdk/Contents/Home"
+set -gx JAVA_HOME "/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home"
 set -gx SCALA_HOME "$dev/scala-2.11.1"
-
-set -gx DOCKER_HOST "tcp://192.168.59.103:2376"
-set -gx DOCKER_CERT_PATH "/Users/netzwerg/.boot2docker/certs/boot2docker-vm"
-set -gx DOCKER_TLS_VERIFY "1"
 
 set -gx GRADLE_OPTS "-Dfile.encoding=UTF-8"
 
@@ -28,6 +24,9 @@ alias vi=vim
 alias ll='ls -alh'
 alias st=stree
 alias find-note='mdfind -name notes.txt -onlyin ~ -0 | xargs -0 grep -i'
+
+# Docker machine initialization
+eval (docker-machine env)
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
