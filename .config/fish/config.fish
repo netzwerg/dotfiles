f9aed2a0d434c5e3c70c3ff09ad9544e9cad727e
Rahel Lüthy <rahel.luethy@fhnw.ch>
2021-01-12 14:33:49 +0100

    Backup recent fish/git/iterm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 set homebrew  "/usr/local/bin"
 set netzwerg  "$HOME/bin"
 set dev "$HOME/Dev"
+set browser "Firefox Developer Edition"
 
 set -gx EDITOR "vim"
 
@@ -14,8 +15,11 @@ alias vi=vim
 alias ll='ls -alh'
 alias gp='git push'
 alias ns='npm run start'
+alias ys='yarn start'
 alias ni='npm install'
+alias yi='yarn install'
 alias nt='npm run test'
+alias yt='yarn test'
 alias find-note='mdfind -name notes.txt -onlyin ~ -0 | xargs -0 grep -i'
 alias scala="command scala -Dscala.color"
 
